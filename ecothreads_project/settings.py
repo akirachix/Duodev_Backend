4a36e4d1d7555a8171063a90638fef1cca33c1d2
mcgarretronald <mcgarretronald@gmail.com>
2024-09-16 17:20:08 +0300

    db

--- ecothreads_project/settings.py
+++ ecothreads_project/settings.py
@@ -128,9 +128,9 @@ WSGI_APPLICATION = 'ecothreads_project.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": "mydatabase",
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 # DATABASES = {
